aab20eb528a63b79bf8aae7c92dc407b577dc543
mylinh202 <btmlinh202@gmail.com>
2025-03-23 19:34:34 +0700

    update setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Pages from "./components/pages/page";
+
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-    <h1 className="text-4xl font-bold text-blue-500">Hello Tailwind CSS!</h1>
-  </div>
+    <h1 className="App">
+      <Pages></Pages>
+    </h1>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  fontFamily: {
-    roboto: ['Roboto', 'sans-serif'],
-  },
   theme: {
     extend: {},
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif'],
+    },
   },
   plugins: [],
-}
+});
